56c235ddaf804a5b8e8b87492e4e07a6a4445adc
Željko Filipin <zeljko.filipin@gmail.com>
2018-10-23 16:38:17 +0200

    The initial Selenium script

---
+++ phabricator-gerrit.rb
@@ -0,0 +1,10 @@
+require "selenium-webdriver"
+
+driver = Selenium::WebDriver.for :firefox
+driver.navigate.to "https://phabricator.wikimedia.org/T188381"
+
+elements = driver.find_elements(class: 'remarkup-link')
+
+puts elements
+
+driver.quit
