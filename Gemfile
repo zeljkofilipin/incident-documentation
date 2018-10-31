0ef32623af81ae23793f2c83510b4ea213f7c76f
Željko Filipin <zeljko.filipin@gmail.com>
2018-10-31 12:10:23 +0100

    Get repository from Gerrit patch

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'mediawiki_api', '~> 0.7.1'
 gem 'rubocop', '~> 0.59.2'
+gem 'selenium-webdriver', '~> 3.14', '>= 3.14.1'
--- wikitext_gerrit_phabricator.rb
+++ wikitext_gerrit_phabricator.rb
@@ -41,4 +41,21 @@ incidents.each do |incident|
   incidents_phabricator[incident] =
     phabricator_from_wikitext(incidents_actionables[incident])
 end
-pp incidents_phabricator
+
+def repository_from_gerrit(patches)
+  require 'selenium-webdriver'
+  patches.map do |patch|
+    driver = Selenium::WebDriver.for :firefox
+    driver.navigate.to "https://gerrit.wikimedia.org/r/#/c/#{patch}/"
+    sleep 2
+    repository = driver.current_url.split('/r/#/c/')[1].split('/+/')[0]
+    driver.quit
+    repository
+  end
+end
+incidents_gerrit_repository = {}
+incidents.each do |incident|
+  incidents_gerrit_repository[incident] =
+    repository_from_gerrit(incidents_gerrit[incident])
+end
+pp incidents_gerrit_repository
