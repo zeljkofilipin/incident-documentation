48feeefa1f13f3658f51518771c60de07cdf00c7
Željko Filipin <zeljko.filipin@gmail.com>
2018-11-05 16:53:19 +0100

    Run Firefox headless

--- lib/gerrit.rb
+++ lib/gerrit.rb
@@ -1,7 +1,8 @@
 def repository_from_gerrit(patches)
   require 'selenium-webdriver'
+  options = Selenium::WebDriver::Firefox::Options.new(args: ['-headless'])
   patches.map do |patch|
-    driver = Selenium::WebDriver.for :firefox
+    driver = Selenium::WebDriver.for(:firefox, options: options)
     patch_url = "https://gerrit.wikimedia.org/r/#/c/#{patch}/"
     driver.navigate.to patch_url
     sleep 1 while driver.current_url == patch_url
